60cad6b974b291b886383d4e2c9178f7ab5350c2
seulgiyoon <sgyoon.info@gmail.com>
2022-02-20 17:35:59 +0900

    Add gatsby와 sanity 디폴트 프로젝트 생성

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,42 @@
+require('dotenv').config({
+  path: '.env',
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `까치당`,
+    description: `까치당의 작업을 소개하고 기록을 보관하는 사이트`,
+    twitter: `@kkachidang`,
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        watchMode: true,
+        dataset: 'production',
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
