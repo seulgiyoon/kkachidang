8e4936ed35873cfc628315fdaad1cd60ecda75a4
seulgiyoon <sgyoon.info@gmail.com>
2022-02-25 23:33:55 +0900

    Fix 폰트파일 ttf 추가 및 eot, svg 삭제

--- gatsby/src/styles/Global.ts
+++ gatsby/src/styles/Global.ts
@@ -1,32 +1,26 @@
 import { css } from '@emotion/react';
 
-import regularEot from 'assets/fonts/noto-serif-kr-v17-latin_korean-regular.eot';
-import regularSvg from 'assets/fonts/noto-serif-kr-v17-latin_korean-regular.svg';
-import regularWoff from 'assets/fonts/noto-serif-kr-v17-latin_korean-regular.woff';
-import regularWoff2 from 'assets/fonts/noto-serif-kr-v17-latin_korean-regular.woff2';
-import blackEot from 'assets/fonts/noto-serif-kr-v17-latin_korean-900.eot';
-import blackSvg from 'assets/fonts/noto-serif-kr-v17-latin_korean-900.svg';
-import blackWoff from 'assets/fonts/noto-serif-kr-v17-latin_korean-900.woff';
-import blackWoff2 from 'assets/fonts/noto-serif-kr-v17-latin_korean-900.woff2';
+import regularTtf from 'assets/fonts/noto-serif-kr-regular.ttf';
+import regularWoff from 'assets/fonts/noto-serif-kr-regular.woff';
+import regularWoff2 from 'assets/fonts/noto-serif-kr-regular.woff2';
+import blackTtf from 'assets/fonts/noto-serif-kr-900.ttf';
+import blackWoff from 'assets/fonts/noto-serif-kr-900.woff';
+import blackWoff2 from 'assets/fonts/noto-serif-kr-900.woff2';
 
 const GlobalStyle = css`
-  /* noto-serif-kr-regular - latin_korean */
   @font-face {
     font-family: 'Noto Serif KR';
     font-style: normal;
     font-weight: 400;
-    src: url(${regularEot});
-    src: local(''), url(${regularWoff2}) format('woff2'), url(${regularWoff}) format('woff'),
-      url(${regularSvg}) format('svg');
+    src: local('Noto Serif KR'), url(${regularWoff2}) format('woff2'),
+      url(${regularWoff}) format('woff'), url(${regularTtf}) format('ttf');
   }
-  /* noto-serif-kr-900 - latin_korean */
   @font-face {
     font-family: 'Noto Serif KR';
     font-style: normal;
     font-weight: 900;
-    src: url(${blackEot});
-    src: local(''), url(${blackWoff2}) format('woff2'), url(${blackWoff}) format('woff'),
-      url(${blackSvg}) format('svg');
+    src: local('Noto Serif KR'), url(${blackWoff2}) format('woff2'),
+      url(${blackWoff}) format('woff'), url(${blackTtf}) format('ttf');
   }
 
   * {
