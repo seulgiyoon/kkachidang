c4c08c9094c2332392f3bfbd21d313a0e1c36170
seulgiyoon <sgyoon.info@gmail.com>
2022-02-26 23:43:41 +0900

    Fix 헤더 높이 수치에 상수적용

--- gatsby/src/hooks/useChangeHeaderBgColor.ts
+++ gatsby/src/hooks/useChangeHeaderBgColor.ts
@@ -1,4 +1,5 @@
 import { useSetHeaderBgColor } from 'components/providers/HeaderBgColorProvider';
+import { HEADER_HEIGHT } from 'constants/layout';
 import { RefObject, useCallback, useEffect } from 'react';
 
 const SECTION_THRESHOLD = [0.1, 1];
@@ -22,11 +23,14 @@ const useChangeHeaderBgColor = ({
   const onObserve = useCallback(
     (callback: (color: string) => void) => (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
-        if (entry.boundingClientRect.bottom <= 140 && entry.boundingClientRect.bottom >= 0) {
+        if (
+          entry.boundingClientRect.bottom <= HEADER_HEIGHT &&
+          entry.boundingClientRect.bottom >= 0
+        ) {
           callback(nextColor);
         }
 
-        if (entry.isIntersecting && entry.boundingClientRect.top <= 140) {
+        if (entry.isIntersecting && entry.boundingClientRect.top <= HEADER_HEIGHT) {
           callback(currentColor);
         }
       });
